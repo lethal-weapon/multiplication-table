f93a9c6e0635d0a2961f2d0dc9b0223d99441ed1
Sean Kramer <sean@kramerdesigns.app>
2021-10-19 16:26:10 +0800

    [Multiplication][Sean]feature: add number validity check

---
+++ src/main/java/com/tw/MultiplicationTable.java
@@ -0,0 +1,34 @@
+package com.tw;
+
+public class MultiplicationTable {
+    public String create(int start, int end) {
+        return isValid(start, end) ? generateTable(start, end) : null;
+    }
+
+    public Boolean isValid(int start, int end) {
+        return
+          isInRange(start) &&
+          isInRange(end) &&
+          isStartNotBiggerThanEnd(start, end);
+    }
+
+    public Boolean isInRange(int number) {
+        return number >= 1 && number <= 1000;
+    }
+
+    public Boolean isStartNotBiggerThanEnd(int start, int end) {
+        return start <= end;
+    }
+
+    public String generateTable(int start, int end) {
+        return null;
+    }
+
+    public String generateLine(int start, int row) {
+        return null;
+    }
+
+    public String generateSingleExpression(int multiplicand, int multiplier) {
+        return null;
+    }
+}
