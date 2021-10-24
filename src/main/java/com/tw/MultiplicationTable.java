0d9308ba688d3a293bbb6ed843e417da2c1a7e30
Sean Kramer <sean@kramerdesigns.app>
2021-10-24 17:55:35 +0800

    [Table][Sean]feat: implement multiplication table

--- src/main/java/com/tw/MultiplicationTable.java
+++ src/main/java/com/tw/MultiplicationTable.java
@@ -1,34 +1,48 @@
 package com.tw;
 
 public class MultiplicationTable {
-    public String create(int start, int end) {
-        return isValid(start, end) ? generateTable(start, end) : null;
-    }
+  public String create(int start, int end) {
+    return isValid(start, end)
+      ? generateTable(start, end)
+      : null;
+  }
 
-    public Boolean isValid(int start, int end) {
-        return
-          isInRange(start) &&
-          isInRange(end) &&
-          isStartNotBiggerThanEnd(start, end);
-    }
+  public Boolean isValid(int start, int end) {
+    return
+      isInRange(start) &&
+      isInRange(end) &&
+      isStartNotBiggerThanEnd(start, end);
+  }
 
-    public Boolean isInRange(int number) {
-        return number >= 1 && number <= 1000;
-    }
+  public Boolean isInRange(int number) {
+    return number >= 1 && number <= 1000;
+  }
 
-    public Boolean isStartNotBiggerThanEnd(int start, int end) {
-        return start <= end;
-    }
+  public Boolean isStartNotBiggerThanEnd(int start, int end) {
+    return start <= end;
+  }
 
-    public String generateTable(int start, int end) {
-        return null;
-    }
+  public String generateTable(int start, int end) {
+    StringBuilder table = new StringBuilder();
 
-    public String generateLine(int start, int row) {
-        return null;
-    }
+    for (int i = start; i <= end; i++)
+      table.append(generateLine(start, i)).append("\n");
 
-    public String generateSingleExpression(int multiplicand, int multiplier) {
-        return null;
-    }
+    return table.toString().trim();
+  }
+
+  public String generateLine(int start, int row) {
+    StringBuilder line = new StringBuilder();
+
+    for (int i = start; i <= row; i++)
+      line.append(generateSingleExpression(i, row)).append("  ");
+
+    return line.toString().trim();
+  }
+
+  public String generateSingleExpression(int multiplicand, int multiplier) {
+    return String.format(
+      "%d*%d=%d", multiplicand, multiplier, multiplicand * multiplier
+    );
+  }
 }
